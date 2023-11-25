87642cdf22a1eb6619d6197267a9dfd2f2870c6c
Mauro Arantes <mauro.arantes@gmail.com>
2023-11-25 12:59:57 +0000

    Initial Commit

---
+++ TicTacToe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TicTacToe
+//
+//  Created by Mauro Arantes on 25/11/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
